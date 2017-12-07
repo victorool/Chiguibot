61e863baf35c4f83df38ea02f1756df4ce09e61e
victorool <30991081+victorool@users.noreply.github.com>
2017-12-07 19:01:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 import json
 import discord
 
-from chiguibot.database import Database
+
 from chiguibot.hotslogs_api import HotsLogsAPI
 
 from chiguibot.cogs.general.cog import GeneralCommands
@@ -21,13 +21,8 @@ def on_ready():
     print(client.user.id)
     print('------')
     
-db = Database(uri)
-asyncio.get_event_loop().run_until_complete(db.test_connection())
 
 api = HotsLogsAPI()
 
-client.add_cog(GeneralCommands(bot))
-client.add_cog(PickupCommands(bot, api, db, channel))
-client.add_cog(UserCommands(bot, api, db))
 
 client.run('Mzg4NDM1MTE2ODQzNzI4OTA4.DQtDxw.L7r-HWHMzca_ZHpWwWyQus_K4Qg')
