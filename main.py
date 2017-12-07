057e4967a9a1d6ca6079a5d7b86a1ffd27eb1e31
victorool <30991081+victorool@users.noreply.github.com>
2017-12-07 17:50:53 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+import asyncio
+import json
+import os
+import discord
+
+from discord.ext.commands import Bot
+
+from chiguibot.database import Database
+from chiguibot.hotslogs_api import HotsLogsAPI
+
+from chiguibot.cogs.general.cog import GeneralCommands
+from chiguibot.cogs.users import UserCommands
+
+bot = Bot(command_prefix="!", description="I am ChiguiBot! I help showing your stats, and do other bot-y things.")
+
+@bot.event
+async def on_ready():
+    print("ChiguiBot: logged in as %s" % (bot.user.name))
+
+token = os.environ["DISCORD_TOKEN"]
+channel = os.environ.get("DISCORD_CHANNEL")
+uri = os.environ["MONGO_URI"]
+
+db = Database(uri)
+asyncio.get_event_loop().run_until_complete(db.test_connection())
+
+api = HotsLogsAPI()
+
+bot.add_cog(GeneralCommands(bot))
+bot.add_cog(PickupCommands(bot, api, db, channel))
+bot.add_cog(UserCommands(bot, api, db))
+
+bot.run(Mzg4NDM1MTE2ODQzNzI4OTA4.DQtDxw.L7r-HWHMzca_ZHpWwWyQus_K4Qg)
