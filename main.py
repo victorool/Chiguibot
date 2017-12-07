22f7df4b81e940eb338d5e9646cbc3ad683fc030
victorool <30991081+victorool@users.noreply.github.com>
2017-12-07 18:19:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import asyncio
 import json
-import os
 import discord
-
-from discord.ext.commands import Bot
 
 from chiguibot.database import Database
 from chiguibot.hotslogs_api import HotsLogsAPI
@@ -11,23 +8,20 @@ from chiguibot.hotslogs_api import HotsLogsAPI
 from chiguibot.cogs.general.cog import GeneralCommands
 from chiguibot.cogs.users import UserCommands
 
-bot = Bot(command_prefix="!", description="I am ChiguiBot! I help showing your stats, and do other bot-y things.")
+client = discord.Client()
 
-@bot.event
+@client.event
 async def on_ready():
-    print("ChiguiBot: logged in as %s" % (bot.user.name))
+    print("ChiguiBot: logged in as %s" % (client.user.name))
 
-token = os.environ["DISCORD_TOKEN"]
-channel = os.environ.get("DISCORD_CHANNEL")
-uri = os.environ["MONGO_URI"]
 
 db = Database(uri)
 asyncio.get_event_loop().run_until_complete(db.test_connection())
 
 api = HotsLogsAPI()
 
-bot.add_cog(GeneralCommands(bot))
-bot.add_cog(PickupCommands(bot, api, db, channel))
-bot.add_cog(UserCommands(bot, api, db))
+client.add_cog(GeneralCommands(bot))
+client.add_cog(PickupCommands(bot, api, db, channel))
+client.add_cog(UserCommands(bot, api, db))
 
-bot.run(Mzg4NDM1MTE2ODQzNzI4OTA4.DQtDxw.L7r-HWHMzca_ZHpWwWyQus_K4Qg)
+client.run(Mzg4NDM1MTE2ODQzNzI4OTA4.DQtDxw.L7r-HWHMzca_ZHpWwWyQus_K4Qg)
